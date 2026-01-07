97a7cb2690f19ebc57b1c3e1185fdde1be90a5e1
C-Nell <nellp7@yahoo.com>
2026-01-07 16:04:59 -0500

    Add documentation for webhook backend plugin
    Added documentation comment for the webhook backend plugin.

--- plugins/webhook-backend/index.ts
+++ plugins/webhook-backend/index.ts
@@ -1,1 +1,10 @@
+/**
+ * Generic Webhook Backend Plugin for Red Hat Developer Hub
+ * 
+ * Provides webhook endpoints to send notifications to RHDH users
+ * from any external service (Ansible AAP, Jenkins, GitHub, etc.)
+ * 
+ * @packageDocumentation
+ */
+
 export { webhookPlugin as default } from './plugin';
